8937756277f45e8e68bb4ee392012ce46a051746
quantick <mail@kladov.xyz>
2021-01-27 14:26:26 +0300

    gchc

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 FROM elixir:1.10.4
 ARG tg_token 
+WORKDIR /app
 COPY . .
 RUN export MIX_ENV=prod && \
     export TG_TOKEN=${tg_token} && \
@@ -7,6 +8,6 @@ RUN export MIX_ENV=prod && \
     mix local.hex --force && \
     mix local.rebar --force && \
     mix deps.get && \
-    mix release && \
-    mkdir -p /opt/app/bin && \
-    cp -R _build/prod/rel/voicehaterbot /opt/app/bin
+    mix release
+
+CMD ["_build/prod/rel/voicehaterbot/bin/voicehaterbot", "start"]
--- lib/hater_worker.ex
+++ lib/hater_worker.ex
@@ -1,19 +1,24 @@
 defmodule HaterWorker do
   use Que.Worker, concurrency: 10
+
   def perform(update) do
     hate(update)
   end
 
   def hate(update) do
-    message = parse_message(update)
-    chat_id = message.chat.id
-    message_id = message.message_id
-    text = message.text
+    %{
+      chat: %{id: chat_id},
+      message_id: message_id,
+      text: text,
+      voice: voice
+    } = parse_message(update)
 
-    case message.voice do
-      nil -> {:ok, :pass}
+    case voice do
+      nil ->
+        {:ok, :pass}
+
       _voice ->
-        Nadia.send_sticker(chat_id, "CAACAgIAAxkBAAMVYBAeFbVA1re0kS22lwy2Y0sVKpUAAl0AA33XWRFpMUFz8IeUXx4E", reply_to_message_id: message_id)
+        Nadia.send_sticker(chat_id, get_sticker(), reply_to_message_id: message_id)
     end
 
     {text, chat_id, message_id}
@@ -22,5 +27,15 @@ defmodule HaterWorker do
   @spec parse_message(%Nadia.Model.Update{}) :: %Nadia.Model.Message{}
   def parse_message(update) do
     update.message || update.channel_post
+  end
+
+  defp get_sticker do
+    stickers = [
+      "CAACAgIAAx0CVbTqhgACByZgESqrXBG35hBqtVlQ_xCdmmfuLwACWwADLEFmGSVEgGM8SxEbHgQ",
+      "CAACAgIAAxkBAAMVYBAeFbVA1re0kS22lwy2Y0sVKpUAAl0AA33XWRFpMUFz8IeUXx4E",
+      "CAACAgIAAx0CVbTqhgACBydgESriLp6Yvemk4a5Xus3-lLZg2QACaAAD_WKhC8FAQIoUsmFiHgQ"
+    ]
+
+    Enum.random(stickers)
   end
 end
